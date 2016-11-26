1a50272077776285207681f99fef30c74e3843b6
Pascal Mueller <pascal.mueller@Pascals-MacBook-Pro.local>
2016-11-26 22:05:09 +0100

    adds more tests

--- src/nivo.slider.spec.js
+++ src/nivo.slider.spec.js
@@ -5,13 +5,35 @@ describe('Nivo Slider', function () {
         loadFixtures('slider.html');
     });
 
-    it('Should find jQuery', function () {
+    it('should find jQuery', function () {
         expect($).not.toBeNull();
     });
 
-    it('has class nivoSlider', function (){
+    it('should have class nivoSlider', function (){
         var slider = $('#slider');
         slider.nivoSlider();
         expect(slider).toHaveClass('nivoSlider');
+    });
+
+    it('should have default navigation labels', function () {
+        var slider = $('#slider');
+        slider.nivoSlider();
+        var navPrev = '.nivo-prevNav';
+        var navNext = '.nivo-nextNav';
+        expect(slider[0].querySelectorAll(navPrev)[0].innerHTML).toBe('Prev');
+        expect(slider[0].querySelectorAll(navNext)[0].innerHTML).toBe('Next');
+    });
+
+    it('should have custom navigation labels', function () {
+        var slider = $('#slider');
+        var config = {
+            prevText: 'Previous',
+            nextText: 'Nextious',
+        };
+        slider.nivoSlider(config);
+        var navPrev = '.nivo-prevNav';
+        var navNext = '.nivo-nextNav';
+        expect(slider[0].querySelectorAll(navPrev)[0].innerHTML).toBe('Previous');
+        expect(slider[0].querySelectorAll(navNext)[0].innerHTML).toBe('Nextious');
     });
 });
